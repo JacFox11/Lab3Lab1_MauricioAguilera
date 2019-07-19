e56098e11655f328bd086a55d037168e076c7f84
JacFox11 <49622962+JacFox11@users.noreply.github.com>
2019-07-19 13:39:42 -0600

    update

--- P3Lab1_MauricioAguilera.cpp
+++ P3Lab1_MauricioAguilera.cpp
@@ -1,1 +1,42 @@
-cout<<"Hello";
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int ** matriz;
+int * arreglo;
+
+int main(){
+	char resp=s;
+	
+	while (resp==s){
+		int op;
+		cout<<"1. Punto de silla"<<endl<<
+				"2. Numeros triangulares"<<endl<<
+				"3. Arreglo de caracteres"<<endl<<
+				"4. Salir"<<endl<<
+				"Ingrese una opcion: ";
+		cin>>op;
+		
+		switch (op){
+			case 1:{
+				
+				break;
+			}
+			
+			case 2:{
+				
+				break;
+			}
+			
+			case 3:{
+				
+				break;
+			}
+			default:{
+				
+				break;
+			}
+		}
+	}
+}
